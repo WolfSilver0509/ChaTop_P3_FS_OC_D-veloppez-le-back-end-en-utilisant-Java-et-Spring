563bb62f0a466c06adae02ee333ef6162190f562
Myriam DENIZOT <myriam.denizot@niji.fr>
2024-04-30 10:06:33 +0200

    init project

---
+++ src/test/java/com/p3_oc_fs/chatop/ChaTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p3_oc_fs.chatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
