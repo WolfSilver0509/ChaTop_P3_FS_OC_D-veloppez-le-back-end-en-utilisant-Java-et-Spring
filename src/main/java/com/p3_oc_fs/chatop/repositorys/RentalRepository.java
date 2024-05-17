ae39c60a437381d57a2c7212a8750462108afbce
Denizot <denizot.myriam@gmail.com>
2024-05-17 20:52:55 +0200

    Ajout @repository  Rental et Message

--- src/main/java/com/p3_oc_fs/chatop/repositorys/RentalRepository.java
+++ src/main/java/com/p3_oc_fs/chatop/repositorys/RentalRepository.java
@@ -2,6 +2,7 @@ package com.p3_oc_fs.chatop.repositorys;
 
 import com.p3_oc_fs.chatop.models.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Interface de dépôt pour la gestion des locations dans la base de données.
@@ -9,5 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * qui fournit des méthodes CRUD (Create, Read, Update, Delete) génériques
  * pour effectuer des opérations sur les entités de type `Rental`.
  */
+@Repository
 public interface RentalRepository extends JpaRepository<Rental, Integer> {
 }
